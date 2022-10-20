b6a5d9ce86b2f8578bd26619921fa142927131df
Kesavan Gnanasekaran <113576759+gkesavan446@users.noreply.github.com>
2022-10-20 13:49:16 +0530

    random delete

--- script.js
+++
@@ -1,8 +0,0 @@
-function foo(){
-    var res=document.getElementById("fname").value;
-    var res1=document.getElementById("mname").value;
-    var res2=document.getElementById("lname").value;
-    var res3=document.getElementById("email").value;
-    var res4=document.getElementById("pass").value;
-    console.log(res, res1, res2, res3, res4,);
-}
